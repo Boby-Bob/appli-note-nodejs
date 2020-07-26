6b3a885e452e08850fdd72c01e04489848e83ff4
Boby-Bob <git@github.com:Boby-Bob>
2020-07-26 04:19:24 +0200

    Modification des couleurs avec chalk

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ yargs
         command: 'list',
         describe: 'List all note--title',
         handler: () => {
-            console.log(chalk.blue('List all note--title'));
+            console.log(chalk.bgBlue('List all note--title'));
             let notes = loadDatas('datas/notes.json');
             for (let i=0; i < notes.length; i++) {
-                console.log(notes[i].title);
+                console.log(chalk.blue(notes[i].title));
             }
         }
     })
@@ -32,6 +32,41 @@ yargs
                 describe: "Add note",
                 demandOption: true,
                 type: "number"
+            }
+        },
+        handler: (argv) => {
+            console.log(chalk.bgBlue("Add note in file"));
+            let notes = loadDatas('datas/notes.json');
+            let exist;
+            for (let i = 0; i<notes.length; i++) {
+                if (notes[i].title.includes(argv.title) == false) {
+                    exist = false;
+                }
+                else {
+                    exist = true;
+                    break;
+                }
+            };
+            if (exist == false) {
+                notes.push({"title" : argv.title, "body" : argv.body});
+                fs.writeFile('datas/notes.json', JSON.stringify(notes), (err) => {
+                    if (err) throw err;
+                    console.log(chalk.green("New note Create"));
+                });
+            }
+            else {
+                console.log(chalk.red(`the ${argv.title} note already exists!`))
+            }
+        }
+    })
+    .command({
+        command: 'remove',
+        describe: 'Remove a note',
+        builder: {
+            title: {
+                describe: "Add the title of the note to delete",
+                demandOption: true,
+                type: "string"
             }
         },
         handler: (argv) => {
